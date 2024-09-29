e4259ff0435d7aa39548f5b9ac1ba203ea1c3af5
callmedukey <iamdevduke@gmail.com>
2024-09-29 16:22:07 +0900

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental:{
+        esmExternals: 'loose',
+    }, output: 'standalone',
+};
 
 export default nextConfig;
